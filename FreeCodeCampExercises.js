c6ee2aa8f22627021fcb0dcdfa8e7f07359c7d3f
Philip N <phi.nguyen@laverne.edu>
2017-07-16 15:25:16 -0700

    Add palindrome

--- FreeCodeCampExercises.js
+++ FreeCodeCampExercises.js
@@ -4,3 +4,10 @@ function reverseString(str) {
   return result;  ///notice the separator for split and join is an empty str.
 }
 reverseString("hello");
+
+
+function palindrome (str) {
+  var editedStr = str.toLowerCase().replace(/[\W_]/g, '');///use regex for a pattern match. when that pattern found in edited str, replace...
+  return editedStr === editedStr.split('').reverse().join(''); ///with an empty string (and therefore eliminating that).
+}
+palindrome("eye");
